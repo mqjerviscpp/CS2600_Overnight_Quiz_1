ae2edac0940f781ec79d6d34ff4fa48de13d1931
mqjerviscpp <mqjervis@cpp.edu>
2023-10-27 03:34:58 -0700

    Commit search-multiple-function-files
    Includes all required files and output image

---
+++ search-multiple-function/EmployeeOne.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include "employee.h"
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber){
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)  //search until end of table  ptr++ will increment by what??
+    {
+        if(ptr->number == targetNumber) //check if it equals the Employee number
+            return (PtrToEmployee) ptr; //return location of the number to callee.
+    }
+     return NULL;   //this will only happen if no Employee number matches in loop above
+}
+
+//Essentially the same functionality as above but comparing strings to check if equal
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char * targetName){
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)
+    {
+        if(strcmp(ptr->name,targetName) == 0)
+            return (PtrToEmployee) ptr;
+    }
+    return NULL;
+}
+
+//search function for phone number
+PtrToEmployee searchEmployeeByPhoneNum(PtrToConstEmployee ptr, int tableSize, char * phoneNum){
+    const PtrToConstEmployee endPtr = (ptr+tableSize);
+    for(; ptr < endPtr; ptr++){
+        if(strcmp(ptr->phone, phoneNum)==0){
+            return (PtrToEmployee) ptr;
+        }
+    }
+    return NULL;
+}
+
+//search function for salary
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double salary){
+    const PtrToConstEmployee endPtr = (ptr+tableSize);
+    for(; ptr<endPtr; ptr++){
+        if(ptr->salary == salary){
+            return (PtrToEmployee) ptr;
+        }
+    }
+    return NULL;
+}
